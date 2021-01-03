33bd75d456670257a94c6ed62fa8e6003cc61d48
Melvin Kisten <iammelvink@gmail.com>
2021-01-04 00:39:08 +0200

    Serving Products from the backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+const port = 8000
+
+// get test route
+app.get('/', (req, res) => {
+	res.send('API is running...')
+})
+// get test route for products
+app.get('/api/products', (req, res) => {
+	res.json(products)
+})
+// get test route for product by id
+app.get('/api/product/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id)
+	res.json(product)
+})
+
+app.listen(port, console.log(`Server running on port ${port}`))
