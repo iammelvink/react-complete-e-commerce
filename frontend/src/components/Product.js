9701aef8b172af155fc6a2a26ce9c5d5bb568b34
Melvin Kisten <iammelvink@gmail.com>
2021-01-04 00:05:30 +0200

    Product Details Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,20 +7,24 @@ const Product = ({ product }) => {
 	return (
 		<Card className='my-3 p-3 rounded'>
 			<Link to={`/product/${product._id}`}>
+				{/* Product image */}
 				<Card.Img src={product.image} variant='top' />
 			</Link>
 			<Card.Body>
 				<Link to={`/product/${product._id}`}>
+					{/* Product name */}
 					<Card.Title as='div'>
 						<strong>{product.name}</strong>
 					</Card.Title>
 				</Link>
 				<Card.Text as='div'>
+					{/* Product rating */}
 					<Rating
 						value={product.rating}
 						text={`${product.numReviews} reviews`}
 					/>
 				</Card.Text>
+				{/* Product price */}
 				<Card.Text as='h3'>R{product.price}</Card.Text>
 			</Card.Body>
 		</Card>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,80 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
-	return <>Product</>
+const ProductScreen = ({ match }) => {
+	// Find product by id
+	const product = products.find((p) => p._id === match.params.id)
+	return (
+		<>
+			{/* Back button */}
+			<Link className='btn btn-light my-3' to='/'>
+				Go Back
+			</Link>
+			<Row>
+				{/* Product image */}
+				<Col md='6'>
+					<Image src={product.image} alt={product.name} fluid />
+				</Col>
+				<Col md='3'>
+					{/* Product name */}
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						{/* Product rating */}
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						{/* Product price */}
+						<ListGroup.Item>Price: R{product.price}</ListGroup.Item>
+						{/* Product description */}
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				{/* Add to cart section */}
+				<Col md='3'>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								{/* Product price */}
+								<Row>
+									<Col>Price:</Col>
+									<Col>
+										<strong>R{product.price}</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							{/* Product status */}
+							<ListGroup.Item>
+								<Row>
+									<Col>Status:</Col>
+									<Col>
+										{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							{/* Add to cart button */}
+							<ListGroup.Item>
+								<Button
+									className='btn-block'
+									type='button'
+									disabled={product.countInStock === 0}
+								>
+									Add To Cart
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	)
 }
 
 export default ProductScreen
