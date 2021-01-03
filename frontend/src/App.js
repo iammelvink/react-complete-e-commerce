1db57e8d6efed4d38598a379624b695d670ad074
Melvin Kisten <iammelvink@gmail.com>
2021-01-03 21:31:29 +0200

    React Bootstrap Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
 	return (
 		<>
-			<h1>React E-Commerce</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to React E-Commerce</h1>
+				</Container>
+			</main>
+			<Footer />
 		</>
 	)
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+	return (
+		<footer>
+			<Container>
+				<Row>
+					<Col className='text-center py-3'>
+						Copyright &copy; React E-Commerce
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	)
+}
+
+export default Footer
