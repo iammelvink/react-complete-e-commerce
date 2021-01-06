0d5374d0bfe94278a54dd377204dcb596f8c62d9
Melvin Kisten <iammelvink@gmail.com>
2021-01-07 00:07:07 +0200

    OrderController and Route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 // Mount routes to respective imports
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 // Error middleware for 404
 app.use(notFound)
