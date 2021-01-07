5d44396a4b540f6c0a8730c02b565069a4e552c8
Melvin Kisten <iammelvink@gmail.com>
2021-01-07 14:28:51 +0200

    Order pay reducer and Action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -6,6 +6,9 @@ import {
 	ORDER_DETAILS_REQUEST,
 	ORDER_DETAILS_SUCCESS,
 	ORDER_DETAILS_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS,
+	ORDER_PAY_FAIL,
 } from '../constants/orderConstants'
 
 // Actions to create a new order
@@ -70,6 +73,49 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
 	} catch (error) {
 		dispatch({
 			type: ORDER_DETAILS_FAIL,
+			payload:
+				// Send a custom error message
+				// Else send a generic error message
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		})
+	}
+}
+// Actions to create a new order
+export const payOrder = (orderId, paymentResult) => async (
+	dispatch,
+	getState
+) => {
+	try {
+		dispatch({ type: ORDER_PAY_REQUEST })
+
+		// Get userInfo from userLogin by destructuring
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		// Make put request to make payment
+		const { data } = await axios.put(
+			`/api/orders/${orderId}/pay`,
+			paymentResult,
+			config
+		)
+
+		dispatch({
+			type: ORDER_PAY_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		dispatch({
+			type: ORDER_PAY_FAIL,
 			payload:
 				// Send a custom error message
 				// Else send a generic error message
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,10 @@ import {
 	ORDER_DETAILS_REQUEST,
 	ORDER_DETAILS_SUCCESS,
 	ORDER_DETAILS_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS,
+	ORDER_PAY_FAIL,
+	ORDER_PAY_RESET,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -43,6 +47,24 @@ export const orderDetailsReducer = (
 			}
 		case ORDER_DETAILS_FAIL:
 			return { loading: false, error: action.payload }
+		// In any other case, just return the state
+		default:
+			return state
+	}
+}
+export const orderPayReducer = (state = {}, action) => {
+	switch (action.type) {
+		case ORDER_PAY_REQUEST:
+			return { loading: true }
+		case ORDER_PAY_SUCCESS:
+			return {
+				loading: false,
+				success: true,
+			}
+		case ORDER_PAY_FAIL:
+			return { loading: false, error: action.payload }
+		case ORDER_PAY_RESET:
+			return {}
 		// In any other case, just return the state
 		default:
 			return state
