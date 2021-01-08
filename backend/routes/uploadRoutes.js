80970e0dc91f407d441402040d7494d78863b06a
Melvin Kisten <iammelvink@gmail.com>
2021-01-08 16:39:29 +0200

    Image upload config and endpoint

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,48 @@
+import path from 'path'
+import express from 'express'
+import multer from 'multer'
+const router = express.Router()
+
+const storage = multer.diskStorage({
+	destination(req, file, cb) {
+		cb(null, 'uploads/')
+	},
+	filename(req, file, cd) {
+		cb(
+			null,
+			// Set file name to
+			// filename-Date.now().extension
+			`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+		)
+	},
+})
+
+function checkFileType(file, cb) {
+	// Accepted file types
+	const filetypes = /jpg|jpeg|png/
+
+	// Check if upload file type matches with accepted file types
+	const extName = filetypes.test(
+		path.extname(file.originalname).toLocaleLowerCase()
+	)
+
+	const mimetype = filetypes.test(file.mimetype)
+	if (extName && mimetype) {
+		return cb(null, true)
+	} else {
+		cb('Images only!')
+	}
+}
+
+const upload = multer({
+	storage,
+	fileFilter: function (req, file, cb) {
+		checkFileType(file, cb)
+	},
+})
+
+router.post('/', upload.single('image'), (req, res) => {
+	res.send(`/${req.file.path}`)
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express, { json } from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -7,6 +8,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -26,10 +28,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+// Make uploads folder static
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // Error middleware for 404
 app.use(notFound)
