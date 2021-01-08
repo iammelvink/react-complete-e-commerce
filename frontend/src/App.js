b935f19ab51739941bf3e28b8405aaa0042e56fe
Melvin Kisten <iammelvink@gmail.com>
2021-01-08 15:33:58 +0200

    Edit Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
 	return (
@@ -48,6 +49,8 @@ const App = () => {
 					<Route path='/admin/user/:id/edit' component={UserEditScreen} />
 					{/* ProductListScreen */}
 					<Route path='/admin/productlist' component={ProductListScreen} />
+					{/* ProductEditScreen */}
+					<Route path='/admin/product/:id/edit' component={ProductEditScreen} />
 					{/* HomeScreen */}
 					<Route path='/' component={HomeScreen} exact />
 				</Container>
---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
+
+const ProductScreen = ({ match, history }) => {
+	const productId = match.params.id
+
+	const [name, setName] = useState('')
+	const [price, setPrice] = useState(0)
+	const [image, setImage] = useState('')
+	const [brand, setBrand] = useState('')
+	const [countInStock, setCountInStock] = useState(0)
+	const [category, setCategory] = useState('')
+	const [description, setDescription] = useState('')
+
+	const dispatch = useDispatch()
+
+	// useSelector is to grab what we want from the state
+	const productDetails = useSelector((state) => state.productDetails)
+	const { loading, error, product } = productDetails
+
+	// make request here upon component load
+	useEffect(() => {
+		if (!product.name || product._id !== productId) {
+			dispatch(listProductDetails(productId))
+		} else {
+			setName(product.name)
+			setPrice(product.price)
+			setImage(product.image)
+			setBrand(product.brand)
+			setCountInStock(product.countInStock)
+			setCategory(product.category)
+			setDescription(product.description)
+		}
+	}, [dispatch, history, product, productId]) // Dependencies, on change they fire off useEffect)
+
+	const submitHandler = (e) => {
+		e.preventDefault()
+		dispatch()
+		// updateProduct({
+		// 	_id: productId,
+		// 	name,
+		// 	price,
+		// 	image,
+		// 	brand,
+		// 	countInStock,
+		// 	category,
+		// 	description,
+		// })
+	}
+
+	return (
+		<>
+			<Link to='/admin/productlist' className='btn btn-light my-3'>
+				Go Back
+			</Link>
+			<FormContainer>
+				<h1>Edit Product</h1>
+				{/* On error, display message/error
+            When loading, display Loading... */}
+				{loading ? (
+					<Loader />
+				) : error ? (
+					<Message variant='danger'>{error}</Message>
+				) : (
+					<Form onSubmit={submitHandler}>
+						{/* Name */}
+						<Form.Group controlId='email'>
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Enter name'
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Price */}
+						<Form.Group controlId='price'>
+							<Form.Label>Price</Form.Label>
+							<Form.Control
+								type='number'
+								min='0'
+								step='0.01'
+								placeholder='Enter price'
+								value={price}
+								onChange={(e) => setPrice(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Image */}
+						<Form.Group controlId='image'>
+							<Form.Label>Image</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Enter image url'
+								value={image}
+								onChange={(e) => setImage(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Brand */}
+						<Form.Group controlId='brand'>
+							<Form.Label>Brand</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Enter brand'
+								value={brand}
+								onChange={(e) => setBrand(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Count in Stock */}
+						<Form.Group controlId='countInStock'>
+							<Form.Label>Count In Stock</Form.Label>
+							<Form.Control
+								type='number'
+								min='0'
+								step='1'
+								placeholder='Enter count in stock'
+								value={countInStock}
+								onChange={(e) => setCountInStock(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Category */}
+						<Form.Group controlId='category'>
+							<Form.Label>Category</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Enter category'
+								value={category}
+								onChange={(e) => setCategory(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Description */}
+						<Form.Group controlId='description'>
+							<Form.Label>Description</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Enter description'
+								value={description}
+								onChange={(e) => setDescription(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						{/* Button */}
+						<Button type='submit' variant='primary'>
+							Update
+						</Button>
+					</Form>
+				)}
+			</FormContainer>
+		</>
+	)
+}
+
+export default ProductScreen
