71ce1a5f33c1dcf6fedf001b52e5ff36d3722720
Sairam <deekondasairam017@gmail.com>
2025-07-12 22:11:12 +0530

    completed

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,251 +1,208 @@
 import axios from 'axios'
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 import {
-	ORDER_CREATE_REQUEST,
-	ORDER_CREATE_SUCCESS,
-	ORDER_CREATE_FAIL,
-	ORDER_DETAILS_REQUEST,
-	ORDER_DETAILS_SUCCESS,
-	ORDER_DETAILS_FAIL,
-	ORDER_PAY_REQUEST,
-	ORDER_PAY_SUCCESS,
-	ORDER_PAY_FAIL,
-	ORDER_LIST_MY_REQUEST,
-	ORDER_LIST_MY_SUCCESS,
-	ORDER_LIST_MY_FAIL,
-	ORDER_LIST_REQUEST,
-	ORDER_LIST_SUCCESS,
-	ORDER_LIST_FAIL,
-	ORDER_DELIVER_REQUEST,
-	ORDER_DELIVER_SUCCESS,
-	ORDER_DELIVER_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
+  ORDER_LIST_FAIL,
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_FAIL,
 } from '../constants/orderConstants'
 
-// Actions to create a new order
+// Create a new order
 export const createOrder = (order) => async (dispatch, getState) => {
-	try {
-		dispatch({ type: ORDER_CREATE_REQUEST })
+  try {
+    dispatch({ type: ORDER_CREATE_REQUEST })
 
-		// Get userInfo from userLogin by destructuring
-		const {
-			userLogin: { userInfo },
-		} = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-		const config = {
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${userInfo.token}`,
-			},
-		}
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		// Make post request to create an order
-		const { data } = await axios.post('/api/orders', order, config)
+    const { data } = await axios.post('/api/orders', order, config)
 
-		dispatch({
-			type: ORDER_CREATE_SUCCESS,
-			payload: data,
-		})
-		dispatch({
-			type: CART_CLEAR_ITEMS,
-			payload: data,
-		})
-		localStorage.removeItem('cartItems')
-	} catch (error) {
-		dispatch({
-			type: ORDER_CREATE_FAIL,
-			payload:
-				// Send a custom error message
-				// Else send a generic error message
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		})
-	}
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data })
+    dispatch({ type: CART_CLEAR_ITEMS, payload: data })
+    localStorage.removeItem('cartItems')
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
-// Actions to create a new order
+
+// Get order details
 export const getOrderDetails = (id) => async (dispatch, getState) => {
-	try {
-		dispatch({ type: ORDER_DETAILS_REQUEST })
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST })
 
-		// Get userInfo from userLogin by destructuring
-		const {
-			userLogin: { userInfo },
-		} = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-		const config = {
-			headers: {
-				Authorization: `Bearer ${userInfo.token}`,
-			},
-		}
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		// Make get request to get an order
-		const { data } = await axios.get(`/api/orders/${id}`, config)
+    const { data } = await axios.get(`/api/orders/${id}`, config)
 
-		dispatch({
-			type: ORDER_DETAILS_SUCCESS,
-			payload: data,
-		})
-	} catch (error) {
-		dispatch({
-			type: ORDER_DETAILS_FAIL,
-			payload:
-				// Send a custom error message
-				// Else send a generic error message
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		})
-	}
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
-// Actions to create a new order
-export const payOrder = (orderId, paymentResult) => async (
-	dispatch,
-	getState
-) => {
-	try {
-		dispatch({ type: ORDER_PAY_REQUEST })
 
-		// Get userInfo from userLogin by destructuring
-		const {
-			userLogin: { userInfo },
-		} = getState()
+// Pay for an order
+export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_PAY_REQUEST })
 
-		const config = {
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${userInfo.token}`,
-			},
-		}
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-		// Make put request to make payment
-		const { data } = await axios.put(
-			`/api/orders/${orderId}/pay`,
-			paymentResult,
-			config
-		)
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		dispatch({
-			type: ORDER_PAY_SUCCESS,
-			payload: data,
-		})
-	} catch (error) {
-		dispatch({
-			type: ORDER_PAY_FAIL,
-			payload:
-				// Send a custom error message
-				// Else send a generic error message
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		})
-	}
+    const { data } = await axios.put(
+      `/api/orders/${orderId}/pay`,
+      paymentResult,
+      config
+    )
+
+    dispatch({ type: ORDER_PAY_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_PAY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
-// Actions to list my orders
+
+// List user's orders
 export const listMyOrders = () => async (dispatch, getState) => {
-	try {
-		dispatch({ type: ORDER_LIST_MY_REQUEST })
+  try {
+    dispatch({ type: ORDER_LIST_MY_REQUEST })
 
-		// Get userInfo from userLogin by destructuring
-		const {
-			userLogin: { userInfo },
-		} = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-		const config = {
-			headers: {
-				Authorization: `Bearer ${userInfo.token}`,
-			},
-		}
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		// Make get request to get my orders
-		const { data } = await axios.get('/api/orders/myorders', config)
+    const { data } = await axios.get('/api/orders/myorders', config)
 
-		dispatch({
-			type: ORDER_LIST_MY_SUCCESS,
-			payload: data,
-		})
-	} catch (error) {
-		dispatch({
-			type: ORDER_LIST_MY_FAIL,
-			payload:
-				// Send a custom error message
-				// Else send a generic error message
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		})
-	}
+    dispatch({ type: ORDER_LIST_MY_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
-// Actions to list all orders
+
+// List all orders (admin)
 export const listOrders = () => async (dispatch, getState) => {
-	try {
-		dispatch({ type: ORDER_LIST_REQUEST })
+  try {
+    dispatch({ type: ORDER_LIST_REQUEST })
 
-		// Get userInfo from userLogin by destructuring
-		const {
-			userLogin: { userInfo },
-		} = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-		const config = {
-			headers: {
-				Authorization: `Bearer ${userInfo.token}`,
-			},
-		}
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		// Make get request to get all orders
-		const { data } = await axios.get('/api/orders', config)
+    const { data } = await axios.get('/api/orders', config)
 
-		dispatch({
-			type: ORDER_LIST_SUCCESS,
-			payload: data,
-		})
-	} catch (error) {
-		dispatch({
-			type: ORDER_LIST_FAIL,
-			payload:
-				// Send a custom error message
-				// Else send a generic error message
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		})
-	}
+    dispatch({ type: ORDER_LIST_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
-// Actions to create a new order
-export const deliverOrder = (order) => async (dispatch, getState) => {
-	try {
-		dispatch({ type: ORDER_DELIVER_REQUEST })
 
-		// Get userInfo from userLogin by destructuring
-		const {
-			userLogin: { userInfo },
-		} = getState()
+// Update order to delivered (admin)
+export const deliverOrder = (orderId) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_DELIVER_REQUEST })
 
-		const config = {
-			headers: {
-				Authorization: `Bearer ${userInfo.token}`,
-			},
-		}
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-		// Make put request to mark order as out for delivery
-		const { data } = await axios.put(
-			`/api/orders/${order._id}/deliver`,
-			{},
-			config
-		)
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		dispatch({
-			type: ORDER_DELIVER_SUCCESS,
-			payload: data,
-		})
-	} catch (error) {
-		dispatch({
-			type: ORDER_DELIVER_FAIL,
-			payload:
-				// Send a custom error message
-				// Else send a generic error message
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		})
-	}
+    const { data } = await axios.put(
+      `/api/orders/${orderId}/deliver`,
+      {},
+      config
+    )
+
+    dispatch({ type: ORDER_DELIVER_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: ORDER_DELIVER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -1,88 +1,108 @@
 import React, { useEffect } from 'react'
-
-import { Table } from 'react-bootstrap'
-import { FaCheckCircle } from 'react-icons/fa';
+import { Table, Button } from 'react-bootstrap'
+import { FaCheckCircle, FaTimes } from 'react-icons/fa'
 import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listOrders } from '../actions/orderActions'
+import { listOrders, deliverOrder } from '../actions/orderActions'
 
 const OrderListScreen = ({ history }) => {
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-	const orderList = useSelector((state) => state.orderList)
-	const { loading, error, orders } = orderList
+  const orderList = useSelector((state) => state.orderList)
+  const { loading, error, orders } = orderList
 
-	const userLogin = useSelector((state) => state.userLogin)
-	const { userInfo } = userLogin
+  const orderDeliver = useSelector((state) => state.orderDeliver)
+  const { loading: loadingDeliver, success: successDeliver } = orderDeliver
 
-	useEffect(() => {
-		if (userInfo && userInfo.isAdmin) {
-			dispatch(listOrders())
-		} else {
-			history.push('/login')
-		}
-	}, [dispatch, userInfo, history])
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-	return (
-		<>
-			<h1>Orders</h1>
-			{loading ? (
-				<Loader />
-			) : error ? (
-				<Message variant='danger'>{error}</Message>
-			) : (
-				<Table bordered hover responsive className='table-sm'>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>User</th>
-							<th>Date</th>
-							<th>Total</th>
-							<th>Paid</th>
-							<th>Delivered</th>
-							{/* <th>Info</th> removed */}
-						</tr>
-					</thead>
-					<tbody>
-						{orders.map((order) => (
-							<tr key={order._id}>
-								<td>{order._id}</td>
-								<td>{order.user && order.user.name}</td>
-								<td>{order.createdAt.substring(0, 10)}</td>
-								<td>R{order.totalPrice}</td>
-<td>
-  {/* Debug: Log order object and order._id for troubleshooting */}
-  {console.log('Order row:', order)}
-  {order.isPaid ? (
-	<span style={{ color: 'green', fontWeight: 600 }}>
-	  <FaCheckCircle style={{ marginRight: 4 }} /> Paid
-	</span>
-  ) : (
-	<span style={{ color: 'red', fontWeight: 600 }}>
-	  <i className='fas fa-times' style={{ marginRight: 4 }}></i> Not Paid
-	</span>
-  )}
-</td>
-<td>
-  {order.isDelivered ? (
-	<span style={{ color: 'green', fontWeight: 600 }}>
-	  <FaCheckCircle style={{ marginRight: 4 }} /> Delivered
-	</span>
-  ) : (
-	<span style={{ color: 'red', fontWeight: 600 }}>
-	  <i className='fas fa-times' style={{ marginRight: 4 }}></i> Not Delivered
-	</span>
-  )}
-</td>
-								{/* Details button removed */}
-							</tr>
-						))}
-					</tbody>
-				</Table>
-			)}
-		</>
-	)
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listOrders())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history, userInfo, successDeliver])
+
+  const deliverHandler = (orderId) => {
+    if (window.confirm('Are you sure you want to mark this order as delivered?')) {
+      dispatch(deliverOrder(orderId))
+    }
+  }
+
+  return (
+    <>
+      <h1>Orders</h1>
+      {loadingDeliver && <Loader />}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>R{order.totalPrice.toFixed(2)}</td>
+                <td>
+                  {order.isPaid ? (
+                    <span style={{ color: 'green' }}>
+                      <FaCheckCircle /> {order.paidAt.substring(0, 10)}
+                    </span>
+                  ) : (
+                    <span style={{ color: 'red' }}>
+                      <FaTimes />
+                    </span>
+                  )}
+                </td>
+                <td>
+                  {order.isDelivered ? (
+                    <span style={{ color: 'green' }}>
+                      <FaCheckCircle /> {order.deliveredAt.substring(0, 10)}
+                    </span>
+                  ) : (
+                    <span style={{ color: 'red' }}>
+                      <FaTimes />
+                    </span>
+                  )}
+                </td>
+                <td>
+                  {/* Details button removed for admin */}
+                  {!order.isDelivered && (
+                    <Button
+                      variant='success'
+                      className='btn-sm'
+                      onClick={() => deliverHandler(order._id)}
+                      disabled={loadingDeliver}
+                    >
+                      Mark Delivered
+                    </Button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  )
 }
 
 export default OrderListScreen
